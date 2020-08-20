5540d72456d58d81320b5d0e6309ae1d9d255c28
Josh Weeks <weeks.josh@gmail.com>
2020-08-20 00:23:48 -0400

    work out some iso grid positioning

---
+++ Assets/Scripts/BustAKludController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BustAKludController : MonoBehaviour
+{
+    public float kludHalfHeight = -0.25f;
+    public float kludHalfWidth = 0.2f;
+    public int columnCount = 6;
+
+    public bool dropEnabled = true;
+    public float dropPeriodSeconds = 4f;
+
+    public Crusher crusher;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _board = new GameObject[columnCount, 32];
+        this.StartCoroutine(this.DropPeriodically());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Dock(GameObject klud)
+    {
+        var kludTransform = klud.GetComponent<Transform>();
+        var pos = kludTransform.localPosition;
+        var left = pos.x;
+
+        // round to nearest width
+        var roundFactor = 1f / kludHalfWidth;
+        left -= kludHalfWidth;
+        left = Mathf.Round(left * roundFactor) / roundFactor;
+        left += kludHalfWidth;
+        left = Mathf.Clamp(left, kludHalfWidth, kludHalfWidth * 2f * columnCount + kludHalfWidth);
+        Debug.Log($"Shifted from {pos.x} to {left} ({roundFactor})");
+        kludTransform.localPosition = new Vector3(left, kludHalfHeight, pos.z);
+    }
+
+    private GameObject[,] _board;
+
+    private IEnumerator DropPeriodically()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(this.dropPeriodSeconds);
+            if (this.dropEnabled)
+            {
+                this.crusher.Drop();
+            }
+        }
+    }
+}
--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -7,15 +7,15 @@ public class Crusher : MonoBehaviour
     public AnimationCurve dropCurve;
     public float fallSpeed = 20.0f;
     public Vector2 fallDistance = new Vector2(0, -1);
+    public BustAKludController manager;
 
     private Vector3 targetPosition;
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.targetPosition = this.transform.position;
-        Debug.Log("Start position: " + this.targetPosition);
-        this.StartCoroutine(this.FallOccasionally());
     }
 
     // Update is called once per frame
@@ -26,19 +26,16 @@ public class Crusher : MonoBehaviour
         this.transform.position = Vector3.MoveTowards(this.transform.position, this.targetPosition, step);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         collision.rigidbody.velocity = Vector2.zero;
         collision.rigidbody.isKinematic = true;
+        this.manager.Dock(collision.gameObject);
     }
 
-    private IEnumerator FallOccasionally()
+    public void Drop()
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(4.0f);
-            this.targetPosition = this.transform.position + (Vector3)fallDistance;
-        }
+        this.targetPosition = this.transform.position + (Vector3)fallDistance;
     }
 
 }

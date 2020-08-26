e3741a4b54a872e4b2e81e68d75a328e8d750d6f
Josh Weeks <weeks.josh@gmail.com>
2020-08-26 11:41:28 -0400

    fix reload

--- Assets/Scripts/BustAKlud/NextKludPreview.cs
+++ Assets/Scripts/BustAKlud/NextKludPreview.cs
@@ -4,6 +4,10 @@ public class NextKludPreview : MonoBehaviour
 {
     public void OnNextKludChanged(GameObject kludPrefab)
     {
+        if (this.transform.childCount > 0)
+        {
+            Destroy(this.transform.GetChild(0).gameObject);
+        }
         var previewKlud = Instantiate(kludPrefab);
         previewKlud.transform.SetParent(this.transform);
         previewKlud.transform.localPosition = Vector3.zero;
